4aa992043f92951c971a7f563c6f99cf075c85bf
Squidfishrl <48411473+Squidfishrl@users.noreply.github.com>
2021-04-04 16:43:44 +0300

    fetch stats with api added + more statistics

--- bwStatTracker/packages/exceptions.py
+++ bwStatTracker/packages/exceptions.py
@@ -1,8 +1,20 @@
 
 
 class NoInputError(Exception):
+
     """
     Raised when fetch_stats_api or fetch_stats_no_api is called without
     specifiying uuid or username
     """
+
+    pass
+
+
+class AlreadyLookedUpError(Exception):
+
+    """
+    Raised when Hypixel API blocks viewing the stats when fetch_stats_api is
+    called for the same username/uuid in short periods of time.
+    """
+
     pass
